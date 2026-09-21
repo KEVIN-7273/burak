704297f3a7196b502328a6c3556f55b9e351fdc6
KEVIN <farpistudent1@gmail.com>
2026-09-21 21:22:40 +0900

    feat: Complate MIT TASK M

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,18 @@
-console.log("Hello world!");
+/*&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& MIT TASK M &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&*/
+/*M-TASK
+Shunday function yozing, u raqamlardan tashkil topgan array qabul qilsin 
+va array ichidagi har bir raqam uchun raqamni ozi va hamda osha raqamni 
+kvadratidan tashkil topgan object hosil qilib, hosil bolgan objectlarni 
+array ichida qaytarsin. MASALAN: getSquareNumbers([1, 2, 3])
+ return [{number: 1, square: 1}, {number: 2, square: 4}, {number: 3, square: 9}]*/
+
+function getSquareNumbers(numbers: number[]) {
+  return numbers.map((number) => {
+    return {
+      number: number,
+      square: number * number,
+    };
+  });
+}
+
+console.log(getSquareNumbers([1, 2, 3]));
